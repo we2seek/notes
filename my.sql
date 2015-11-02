b75e929a4073c4e624061ca688ec318a1effacde
Vitaliy Timchenko <vitaliy.timchenko@gmail.com>
2015-11-02 17:12:47 +0200

    add requests examples

--- my.sql
+++ my.sql
@@ -4,3 +4,18 @@ dump
 backup: 	shell> mysqldump -u root -p --databases db_name [db_name2] > dump.sql
 restore:	shell> mysql -uroot -p < dump.sql
 		mysql> source dump.sql
+
+
+
+SELECT r.request_id, AVG(r.value) 
+FROM rate r 
+LEFT JOIN request2master rm ON r.request_id = rm.request_id 
+WHERE rm.master_user_id = 15 AND r.question_id !='comment' 
+GROUP BY r.request_id;
+
+SELECT u.id, u.fio, ROUND(AVG(r.value), 2) AS avg
+FROM rate r
+LEFT JOIN request2master rm ON r.request_id = rm.request_id
+LEFT JOIN user u ON rm.master_user_id = u.id
+WHERE r.question_id != 'comment'
+GROUP BY rm.master_user_id;
